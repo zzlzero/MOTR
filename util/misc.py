5404f60958b83f7a08021de2ea2a5cf3403ed372
zzlzero <1360249883@qq.com>
2023-05-03 11:17:05 +0000

    modified:   util/misc.py 取消torchvision版本检查

--- util/misc.py
+++ util/misc.py
@@ -29,7 +29,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.5:
+'''if float(torchvision.__version__[:3]) < 0.5:
     import math
     from torchvision.ops.misc import _NewEmptyTensorOp
     def _check_size_scale_factor(dim, size, scale_factor):
@@ -58,7 +58,7 @@ if float(torchvision.__version__[:3]) < 0.5:
         ]
 elif float(torchvision.__version__[:3]) < 0.7:
     from torchvision.ops import _new_empty_tensor
-    from torchvision.ops.misc import _output_size
+    from torchvision.ops.misc import _output_size'''
 
 
 class SmoothedValue(object):
